539f0f226c2e7f481824f7d5368d80ab75de4211
Peter Elbaum <elbaumpj@gmail.com>
2017-05-25 11:54:17 -0400

    added css and output component

--- src/app/calculator-output/calculator-output.component.ts
+++ src/app/calculator-output/calculator-output.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, DoCheck, KeyValueDiffers } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { PermitCard } from '../permit-card';
 
 @Component({
-  selector: 'app-calculator-output',
+  selector: 'calculator-output',
   templateUrl: './calculator-output.component.html',
   styleUrls: ['./calculator-output.component.css']
 })
 export class CalculatorOutputComponent implements OnInit {
 
-  constructor() { }
+  @Input() cards: Array<PermitCard>;
+  @Input() cardIndex: number;
+  dailyFeeTotal: number;
+  reviewFeeTotal: number;
+  differ: any;
+  permitcard: PermitCard;
+
+  constructor(private differs: KeyValueDiffers) { 
+    this.differ = differs.find({}).create(null);
+  }
 
   ngOnInit() {
+  }
+
+  ngDoCheck() {
+    let card = this.cards[this.cardIndex];
+
+    this.calcPermit(card);
+  }
+
+  calcPermit(card) {
+    console.log("fee in output comp", card.streetClosureType.reviewFee);
+    console.log("fee in output comp", card.streetClosureType.dailyFee);
   }
 
 }
