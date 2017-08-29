bb6987a88aba5f03bb1d7b8b32dec17b53f54e3f
Peter Elbaum <elbaumpj@gmail.com>
2017-08-29 11:09:41 -0400

    working on calc function for pdf

--- src/app/gather-calc.ts
+++ src/app/gather-calc.ts
@@ -33,71 +33,51 @@ export class GatherCalc {
         //create the map of dates as keys given the date range
         for(var i = 1; i < (diffDays + 2); i++) { 
             
-            let newDate: any = moment(startDate).add(i, 'days');
-            newDate = newDate[Object.keys(newDate)[5]];
-            newDate = moment(newDate).format("YYYY-MM-DD");
-            let matches: any; 
+            let newDate: any = moment(startDate).add(i, 'days'); 
             
             if(this.dateDirectory[newDate]) {
-            if(this.dateDirectory[newDate].daily[this.frontageIndex]) {
-                let object = {index: this.cardIndex,  fee: dailyFee}; 
-                matches = this.dateDirectory[newDate].daily[this.frontageIndex].filter((fee) => fee.index == this.cardIndex);
-            } else {
-                matches = "   "; 
-            }
-            let object = {index: this.cardIndex,  fee: dailyFee};  
-            if(matches.length < 1 && this.dateDirectory[newDate].daily[this.frontageIndex]) {
-                //console.log(this.dateDirectory[newDate].daily[this.frontageIndex]);
-                
-                this.dateDirectory[newDate].daily[this.frontageIndex].push(object);
-            } else {
-                this.dateDirectory[newDate].daily[this.frontageIndex] = [{index: this.cardIndex,  fee: dailyFee}]; 
-                }
-            } else {
-            //console.log('creating new nums'); 
-            this.dateDirectory[newDate] = {
-            daily: {
-                [this.frontageIndex]: [{index: this.cardIndex,  fee: dailyFee}]
+  
+                if(this.dateDirectory[newDate].daily[this.frontageIndex]) {
+                    this.dateDirectory[newDate].daily[this.frontageIndex].push(dailyFee); //? 
+                } else {
+                    this.dateDirectory[newDate].daily[this.frontageIndex] = [dailyFee]; 
                     }
-                }
-            }
+
+            } 
             
             dateDirectoryKeys = Object.keys( this.dateDirectory );
-            //console.log('this is the date directory', this.dateDirectory);
-            
             
         }
 
 
-        dailyFeeTotal = 0; 
+        dailyFeeTotal = 0; //? 
         
-        // let dailyFeesArray: any = []; 
+     
         for(var i = 0; i < dateDirectoryKeys.length; i++) {
-            //console.log('first',this.dateDirectory[dateDirectoryKeys[i]]); 
+           
             this.dailyFeesArray = []; 
             for(var j = 0; j <= this.frontageIndex ; j++) {
             var counter: number = 0;
             if(this.dateDirectory[dateDirectoryKeys[i]].daily[j]) {
                 for(var k = 0; k < this.dateDirectory[dateDirectoryKeys[i]].daily[j].length; k++) {
-                //console.log('date dir with values', this.dateDirectory); 
-                //console.log('thing trying to pushed', this.dateDirectory[dateDirectoryKeys[i]].daily[j]);
+             
                 
                 if(this.dateDirectory[dateDirectoryKeys[i]].daily[j][k].fee > counter) {
                     counter = this.dateDirectory[dateDirectoryKeys[i]].daily[j][k].fee; 
                     this.dailyFeesArray.push(counter);
-                        } 
-                    }  
-                }
-            } 
+                    } 
+                }  
+            }
+        } 
 
             if(this.dailyFeesArray.length == 0) {
             this.dailyFeesArray = [0]; //correcting for potential error with the misc. house moves that have no daily fee
             }
             
-            //console.log('investigating matt error', dailyFeesArray);
+            
             let dailySum: number = this.dailyFeesArray.reduce((prev, curr) => prev + curr);  
             dailyFeeTotal += dailySum; 
-            //console.log(this.dailyFeeTotal); 
+            console.log('this is the daily fee total', dailyFeeTotal); 
         }
     }
 }
